aa5e8de50e102a5aeca59ddf4bc305af79e0cccc
Wittawas Nakkasem <vittee@hotmail.com>
2017-09-28 12:52:54 +0700

    Reformat codes.

--- src/main/java/org/graylog2/logging/GelfHandler.java
+++ src/main/java/org/graylog2/logging/GelfHandler.java
@@ -19,8 +19,7 @@ import java.util.IllegalFormatConversionException;
 import java.util.Map;
 import java.util.logging.*;
 
-public class GelfHandler extends ExtHandler
-{
+public class GelfHandler extends ExtHandler {
     private static final int MAX_SHORT_MESSAGE_LENGTH = 250;
     private static final Map<String, Integer> LOG4J_LOGLEVELS;
 
@@ -67,99 +66,77 @@ public class GelfHandler extends ExtHandler
     private boolean extractStacktrace;
     private Map<String, String> fields;
 
-    public GelfHandler()
-    {
+    public GelfHandler() {
         final LogManager manager = LogManager.getLogManager();
         final String prefix = getClass().getName();
 
-        graylogHost = manager.getProperty( prefix + ".graylogHost" );
-        final String port = manager.getProperty( prefix + ".graylogPort" );
-        graylogPort = null == port ? 12201 : Integer.parseInt( port );
-        originHost = manager.getProperty( prefix + ".originHost" );
-        extractStacktrace = "true".equalsIgnoreCase( manager.getProperty( prefix + ".extractStacktrace" ) );
+        graylogHost = manager.getProperty(prefix + ".graylogHost");
+        final String port = manager.getProperty(prefix + ".graylogPort");
+        graylogPort = null == port ? 12201 : Integer.parseInt(port);
+        originHost = manager.getProperty(prefix + ".originHost");
+        extractStacktrace = "true".equalsIgnoreCase(manager.getProperty(prefix + ".extractStacktrace"));
         int fieldNumber = 0;
-        fields = new HashMap<String, String>(  );
-        while( true )
-        {
-            final String property = manager.getProperty( prefix + ".additionalField." + fieldNumber );
-            if ( null == property )
-            {
+        fields = new HashMap<String, String>();
+        while (true) {
+            final String property = manager.getProperty(prefix + ".additionalField." + fieldNumber);
+            if (null == property) {
                 break;
             }
-            final int index = property.indexOf( '=' );
-            if( -1 != index )
-            {
-                fields.put( property.substring( 0, index ), property.substring( index + 1 ) );
+            final int index = property.indexOf('=');
+            if (-1 != index) {
+                fields.put(property.substring(0, index), property.substring(index + 1));
             }
 
             fieldNumber++;
         }
-        facility = manager.getProperty( prefix + ".facility" );
+        facility = manager.getProperty(prefix + ".facility");
 
 
-        final String level = manager.getProperty( prefix + ".level" );
-        if ( null != level )
-        {
-            setLevel( Level.parse( level.trim() ) );
-        }
-        else
-        {
-            setLevel( Level.INFO );
+        final String level = manager.getProperty(prefix + ".level");
+        if (null != level) {
+            setLevel(Level.parse(level.trim()));
+        } else {
+            setLevel(Level.INFO);
         }
 
-        final String filter = manager.getProperty( prefix + ".filter" );
-        try
-        {
-            if ( null != filter )
-            {
-                final Class clazz = ClassLoader.getSystemClassLoader().loadClass( filter );
-                setFilter( (Filter) clazz.newInstance() );
+        final String filter = manager.getProperty(prefix + ".filter");
+        try {
+            if (null != filter) {
+                final Class clazz = ClassLoader.getSystemClassLoader().loadClass(filter);
+                setFilter((Filter) clazz.newInstance());
             }
-        }
-        catch ( final Exception e )
-        {
+        } catch (final Exception e) {
             //ignore
         }
         //This only used for testing
-        final String testSender = manager.getProperty( prefix + ".graylogTestSenderClass" );
-        try
-        {
-            if ( null != testSender )
-            {
-                final Class clazz = ClassLoader.getSystemClassLoader().loadClass( testSender );
+        final String testSender = manager.getProperty(prefix + ".graylogTestSenderClass");
+        try {
+            if (null != testSender) {
+                final Class clazz = ClassLoader.getSystemClassLoader().loadClass(testSender);
                 gelfSender = (GelfSender) clazz.newInstance();
             }
-        }
-        catch ( final Exception e )
-        {
+        } catch (final Exception e) {
             //ignore
         }
     }
 
     @Override
-    public synchronized void flush()
-    {
+    public synchronized void flush() {
     }
 
 
-    private String getOriginHost()
-    {
-        if ( null == originHost )
-        {
+    private String getOriginHost() {
+        if (null == originHost) {
             originHost = getLocalHostName();
         }
         return originHost;
     }
 
-    private String getLocalHostName()
-    {
-        try
-        {
+    private String getLocalHostName() {
+        try {
             return InetAddress.getLocalHost().getHostName();
-        }
-        catch ( final UnknownHostException uhe )
-        {
-            reportError( "Unknown local hostname", uhe, ErrorManager.GENERIC_FAILURE );
+        } catch (final UnknownHostException uhe) {
+            reportError("Unknown local hostname", uhe, ErrorManager.GENERIC_FAILURE);
         }
 
         return null;
@@ -211,31 +188,27 @@ public class GelfHandler extends ExtHandler
 
 
     @Override
-    public void close()
-    {
-        if ( null != gelfSender )
-        {
+    public void close() {
+        if (null != gelfSender) {
             gelfSender.close();
             gelfSender = null;
         }
     }
 
-    private GelfMessage makeMessage( final LogRecord record )
-    {
+    private GelfMessage makeMessage(final LogRecord record) {
         String message = record.getMessage();
         Object[] parameters = record.getParameters();
 
         if (message == null) message = "";
-        if ( parameters != null && parameters.length > 0 )
-        {
+        if (parameters != null && parameters.length > 0) {
             //by default, using {0}, {1}, etc. -> MessageFormat
-            message = MessageFormat.format( message, parameters );
+            message = MessageFormat.format(message, parameters);
 
-            if(message.equals(record.getMessage())) {
+            if (message.equals(record.getMessage())) {
                 //if the text is the same, assuming this is String.format type log (%s, %d, etc.)
                 try {
-                    message = String.format( message, parameters );
-                } catch (IllegalFormatConversionException e ) {
+                    message = String.format(message, parameters);
+                } catch (IllegalFormatConversionException e) {
                     //leaving message as it is to avoid compatibility problems
                     message = record.getMessage();
                 } catch (NullPointerException e) {
@@ -245,77 +218,56 @@ public class GelfHandler extends ExtHandler
         }
 
         final String shortMessage;
-        if ( message.length() > MAX_SHORT_MESSAGE_LENGTH )
-        {
-            shortMessage = message.substring( 0, MAX_SHORT_MESSAGE_LENGTH - 1 );
-        }
-        else
-        {
+        if (message.length() > MAX_SHORT_MESSAGE_LENGTH) {
+            shortMessage = message.substring(0, MAX_SHORT_MESSAGE_LENGTH - 1);
+        } else {
             shortMessage = message;
         }
 
-        if ( extractStacktrace )
-        {
+        if (extractStacktrace) {
             final Throwable thrown = record.getThrown();
-            if ( null != thrown )
-            {
+            if (null != thrown) {
                 final StringWriter sw = new StringWriter();
-                thrown.printStackTrace( new PrintWriter( sw ) );
+                thrown.printStackTrace(new PrintWriter(sw));
                 message += "\n\r" + sw.toString();
             }
         }
 
-        final GelfMessage gelfMessage =
-                new GelfMessage( shortMessage,
-                        message,
-                        record.getMillis(),
-                        String.valueOf( levelToSyslogLevel( record.getLevel() ) ) );
-        gelfMessage.addField( "SourceClassName", record.getSourceClassName() );
-        gelfMessage.addField( "SourceMethodName", record.getSourceMethodName() );
+        final GelfMessage gelfMessage = new GelfMessage(shortMessage, message, record.getMillis(), String.valueOf(levelToSyslogLevel(record.getLevel())));
+        gelfMessage.addField("SourceClassName", record.getSourceClassName());
+        gelfMessage.addField("SourceMethodName", record.getSourceMethodName());
 
         final String instanceName = System.getProperty("jboss.server.name");
         if (instanceName != null && !instanceName.isEmpty()) {
-           gelfMessage.addField("instanceName", instanceName);
+            gelfMessage.addField("instanceName", instanceName);
         }
 
-        if ( null != getOriginHost() )
-        {
-            gelfMessage.setHost( getOriginHost() );
+        if (null != getOriginHost()) {
+            gelfMessage.setHost(getOriginHost());
         }
 
-        if ( null != facility )
-        {
-            gelfMessage.setFacility( facility );
+        if (null != facility) {
+            gelfMessage.setFacility(facility);
         }
 
-        if ( null != fields )
-        {
-            for ( final Map.Entry<String, String> entry : fields.entrySet() )
-            {
-                gelfMessage.addField( entry.getKey(), entry.getValue() );
+        if (null != fields) {
+            for (final Map.Entry<String, String> entry : fields.entrySet()) {
+                gelfMessage.addField(entry.getKey(), entry.getValue());
             }
         }
 
         return gelfMessage;
     }
 
-    private int levelToSyslogLevel( final Level level )
-    {
+    private int levelToSyslogLevel(final Level level) {
         final int syslogLevel;
-        if ( level == Level.SEVERE )
-        {
+        if (level == Level.SEVERE) {
             syslogLevel = SyslogLevels.ERROR.getLevel();
-        }
-        else if ( level == Level.WARNING )
-        {
+        } else if (level == Level.WARNING) {
             syslogLevel = SyslogLevels.WARNING.getLevel();
-        }
-        else if ( level == Level.INFO )
-        {
+        } else if (level == Level.INFO) {
             syslogLevel = SyslogLevels.INFORMAT.getLevel();
-        }
-        else
-        {
+        } else {
             //try to figure out loglevel
             Integer log4jLevel = LOG4J_LOGLEVELS.get(level.getName());
             syslogLevel = log4jLevel == null ? 7 : log4jLevel;
